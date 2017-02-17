b998c10fc74843be7475c9ec15e257bb78cf9fe1
Abner Simões <abner.simoes@hotmail.com>
2017-02-17 02:47:18 -0200

    Formulário finalizado

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -15,11 +15,13 @@ export class FormComponent implements OnInit {
 
   @ViewChild('form') form;
   @ViewChild('submit') btn;
+  @ViewChild('selectCountry') selectCountry;
 
   formSignup: FormGroup;
   countries: any;
   address: Object;
   body: any;
+  
 
   constructor(
     public titleService: Title,
@@ -31,9 +33,10 @@ export class FormComponent implements OnInit {
       'password':             [null, Validators.compose([Validators.required, Validators.pattern('[a-zA-ZÀ-ú]+')])],
       'passwordConfirmation': [null, Validators.compose([Validators.required])],
       'fullName':             [null, Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(40), Validators.pattern('[a-zA-ZÀ-ú ]+')])],
-      'birthDate':            [null, Validators.compose([Validators.required, Validators.pattern('(?:19|20)[0-9]{2}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))')])],
+      // 'birthDate':            [null, Validators.compose([Validators.required, Validators.pattern('[0-3]{1}[0-9]{1}/[0-1]{1}[0-9]{1}/[0-2]{1}[0-9]{1}[0-9]{1}[0-9]{1}')])],
+      'birthDate':            [null, Validators.compose([Validators.required, Validators.pattern('[0-2]{1}[0-9]{1}[0-9]{1}[0-9]{1}-[0-1]{1}[0-9]{1}-[0-3]{1}[0-9]{1}')])],
       'address':              [null],
-      'zipCode':              [null, Validators.compose([Validators.required, Validators.pattern('[0-9]{5}?[0-9]{3}')])],
+      'zipCode':              [null, Validators.compose([Validators.required, Validators.pattern('[0-9]{5}-?[0-9]{3}')])],
       'streetName':           [null, Validators.compose([Validators.required, Validators.pattern('[a-zA-ZÀ-ú ]+')])],
       'number':               [null, Validators.compose([Validators.required, Validators.pattern('[0-9]+')])],
       'complement':           [null],
@@ -76,54 +79,52 @@ export class FormComponent implements OnInit {
     // title
     this.titleService.setTitle('Form');
   }
-
   
   getAddress(place:any) {       
-    this.address = place['formatted_address'];
-    var location = place['geometry']['location'];
-    var lat =  location.lat();
-    var lng = location.lng();
-    console.log("Address Object", place);
-
-    
-    // this.formSignup.value.zipCode = 
-
-    // console.log(this.formSignup.value);
-
+    // this.address = place['formatted_address'];
+    // var location = place['geometry']['location'];
+    // var lat =  location.lat();
+    // var lng = location.lng();
+    // console.log("Address Object", place);
     place.address_components.forEach(element => {
-      if (element.types == 'postal_code') {
-        this.form.zipCode = element.long_name
-      } else if (element.types == 'route') {
-      
-      } else if(element.types == 'street_number'){
-
-      } else if (element.types == 'country') {
-      // } else if (element.types == '') {
-      // } else if (element.types == '') {
-      // } else if (element.types == '') {
-      // } else if (element.types == '') {
-      // } else if (element.types == '') {
-
+      if (element.types.indexOf('postal_code') > -1) {
+        this.formSignup.controls['zipCode'].patchValue(element.long_name);
+      } else if (element.types.indexOf('route') > -1) {
+        this.formSignup.controls['streetName'].patchValue(element.long_name);
+      } else if (element.types.indexOf('street_number') > -1){
+        this.formSignup.controls['number'].patchValue(element.long_name);        
+      } else if (element.types.indexOf('country') > -1) {
+        this.formSignup.controls['country'].patchValue(element.short_name);                
+      } else if (element.types.indexOf('sublocality') > -1) {
+        this.formSignup.controls['neighbourhood'].patchValue(element.long_name); 
+      } else if (element.types.indexOf('administrative_area_level_1') > -1) {
+        this.formSignup.controls['state'].patchValue(element.short_name);         
+      } else if (element.types.indexOf('administrative_area_level_2') > -1) {
+        this.formSignup.controls['city'].patchValue(element.long_name);
       }
-      // console.log(element.types)
-    });
-
-    console.log(this.form.zipCode)
-
-
-    // Rua Heitor PEnteado, 1929
-    
+    })
   }
 
   onSubmit() {
+    // this.btn.nativeElement.innerHTML = 'Enviando ...';
+
+    // date format
+    let d = this.formSignup.value['birthDate'].substring(8, 10);
+    let m = this.formSignup.value['birthDate'].substring(5, 7);
+    let y = this.formSignup.value['birthDate'].substring(0, 4);
+    this.formSignup.value['birthDate'] = d + '/' + m + '/' + y;
+
+    delete this.formSignup.value['address'];
+    delete this.formSignup.value['passwordConfirmation'];
+
     this.body = JSON.stringify(this.formSignup.value);
-    this.btn.nativeElement.innerHTML = 'Enviando ...';
+    console.log(this.body);
     
-    this.api.postData('http://', this.body)
-      .subscribe(
-        response => this.handleResponse(response),
-        error => this.handleResponse(error)
-      );
+    // this.api.postData('http://', this.body)
+    //   .subscribe(
+    //     response => this.handleResponse(response),
+    //     error => this.handleResponse(error)
+    //   );
   }
 
   handleResponse(response) {
